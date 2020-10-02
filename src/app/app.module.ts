1af74a638601cc5f4b64ccb2fa93f4ad9b50cef7
r3d3vil <shubh98bansal@gmail.com>
2020-10-02 15:58:25 +0530

    Structure ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { MyordersComponent } from './myorders/myorders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WishlistComponent,
+    ProfileComponent,
+    CartComponent,
+    ProductInfoComponent,
+    MyordersComponent
   ],
   imports: [
     BrowserModule,
